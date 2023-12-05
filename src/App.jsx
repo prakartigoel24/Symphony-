e8e062a96eba186573f8b6e9ab352168a8de895f
Prakarti Goel <prakartigoel24@gmail.com>
2023-12-06 00:06:36 +0530

    initial ShazamApi1 setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
-import SearchBar from './components/SearchBar';
-import Navbar from './components/Navbar';
+import React from "react";
+import SearchBar from "./components/SearchBar";
+import Navbar from "./components/Navbar";
+import { useGetGlobalSongsDataQuery } from "./redux/APIs/ShazamApi1";
 
 const App = () => {
+  const { data, isError , isLoading , error} = useGetGlobalSongsDataQuery();
+
   return (
-    <div className='main'>
+    <div className="main">
       <div className="header flex flex-row justify-between items-center">
         <SearchBar />
         <Navbar />
+
+        <div>
+          {!isError ? <>{console.log(data?.tracks)}</> : "Error is there...."}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/redux/APIs/ShazamApi1.jsx
@@ -0,0 +1,27 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+
+const ShazamApi1_Key = import.meta.env.VITE_X_RAPID_API_KEY;
+
+export const ShazamApi1 = createApi({
+  reducerPath: "ShazamApi1",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam.p.rapidapi.com",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        ShazamApi1_Key
+      );
+      return headers;
+    },
+  }),
+  endpoints:(build) => ({
+     getGlobalSongsData: build.query({
+        query: ()=> ({url: '/charts/track'}),
+     })
+  })
+});
+
+export const {
+    useGetGlobalSongsDataQuery,
+} = ShazamApi1;     
