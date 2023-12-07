ff5e7edd1a3b21725d0964394b9219635885cc7c
Prakarti Goel <prakartigoel24@gmail.com>
2023-12-07 14:25:35 +0530

    Removed unncessary endpoints

--- src/redux/APIs/ShazamApi1.jsx
+++ src/redux/APIs/ShazamApi1.jsx
@@ -30,7 +30,4 @@ export const {
   useGetTopSongsQuery,
   useGetTopSongsByGenreQuery,
   useGetTopSongsByCountryQuery,
-  useGetArtistDetailsQuery,
-  useGetRelatedSongsQuery,
-  useGetSongDetailsQuery,
 } = ShazamApi1;
