98a6fd057028c6661ae7b0d7f1dc1818f4d382f1
prakartigoel24 <prakartigoel24@gmail.com>
2023-12-12 21:29:56 +0530

    Music control buttons added (Dummy)

--- src/components/MusicPlayer/Controls.jsx
+++ src/components/MusicPlayer/Controls.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { FaPlay, FaPause } from "react-icons/fa";
+import { IoMdSkipForward, IoMdSkipBackward } from "react-icons/io";
+import { FaShuffle, FaRepeat } from "react-icons/fa6";
+
 
 const Controls = () => {
   return (
-    <div className='bg-yellow-600 grid w-full h-12 items-center justify-center'>Controls</div>
+    <div className='bg-yellow-600 grid grid-flow-col w-full h-12 items-center justify-around'>
+      <FaRepeat  size={20} className=''/>
+      <IoMdSkipBackward size={26} className=''/>
+      <FaPlay size={36} className=''/>
+      <FaPause size={36} className=''/>
+      <IoMdSkipForward size={26} className=''/>
+      <FaShuffle size={20} className=''/>
+    </div>
   )
 }
 
