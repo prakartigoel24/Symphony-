1b897fb4936e4e814b6313c0b778e8afe8846a9d
prakartigoel24 <prakartigoel24@gmail.com>
2023-12-12 21:59:35 +0530

    Added dummy volumebar

--- src/components/MusicPlayer/Volumebar.jsx
+++ src/components/MusicPlayer/Volumebar.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { BsFillVolumeUpFill, BsVolumeDownFill, BsFillVolumeMuteFill } from 'react-icons/bs';
 
 const Volumebar = () => {
   return (
-    <div className='bg-amber-900 w-full hidden md:grid md:col-span-2 md:justify-center md:items-center'>Volumebar</div>
+    <div className='bg-amber-900 w-full hidden md:grid md:col-span-2 md:justify-center md:items-center md:grid-flow-col'>
+    <BsFillVolumeUpFill size={25} color="#FFF" />
+    {/* <BsVolumeDownFill size={25} color="#FFF" /> */}
+    {/* <BsFillVolumeMuteFill size={25} color="#FFF"  /> */}
+    <input
+      type="range"
+      className="lg:w-48 h-1"
+    />
+    </div>
   )
 }
 
