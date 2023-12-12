d64701729cc7e9b6b1e6114df09bdd1bdd78ef45
prakartigoel24 <prakartigoel24@gmail.com>
2023-12-12 21:48:02 +0530

    Resized controls and added dummy seekbar

--- src/components/MusicPlayer/Controls.jsx
+++ src/components/MusicPlayer/Controls.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaPlay, FaPause } from "react-icons/fa";
+import { FaPlayCircle, FaPauseCircle } from "react-icons/fa";
 import { IoMdSkipForward, IoMdSkipBackward } from "react-icons/io";
 import { FaShuffle, FaRepeat } from "react-icons/fa6";
 
@@ -7,12 +7,12 @@ import { FaShuffle, FaRepeat } from "react-icons/fa6";
 const Controls = () => {
   return (
     <div className='bg-yellow-600 grid grid-flow-col w-full h-12 items-center justify-around'>
-      <FaRepeat  size={20} className=''/>
-      <IoMdSkipBackward size={26} className=''/>
-      <FaPlay size={36} className=''/>
-      <FaPause size={36} className=''/>
-      <IoMdSkipForward size={26} className=''/>
-      <FaShuffle size={20} className=''/>
+      <FaRepeat  size={18} className=''/>
+      <IoMdSkipBackward size={25} className=''/>
+      <FaPlayCircle size={36} className=''/>
+      <FaPauseCircle size={36} className=''/>
+      <IoMdSkipForward size={25} className=''/>
+      <FaShuffle size={18} className=''/>
     </div>
   )
 }
--- src/components/MusicPlayer/Seekbar.jsx
+++ src/components/MusicPlayer/Seekbar.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Seekbar = () => {
   return (
-    <div className='bg-blue-500 hidden sm:grid w-full h-8 sm:items-center sm:justify-center'>Seekbar</div>
+    <div className='bg-blue-500 hidden sm:grid w-full h-8 items-center justify-center sm:grid-flow-col'>
+        <p>0.00</p>
+        <input type="range" name="" id="" className='mx-4 w-56 h-1 lg:w-80 xl:w-96' />
+        <p>4.43</p>
+    </div>
   )
 }
 
