04d42096ff449f9d812bcb45f56758371a6a241a
prakartigoel24 <prakartigoel24@gmail.com>
2023-12-12 21:15:59 +0530

    Add: Trackbar layout for displaying song, artist and image

--- src/components/MusicPlayer/Track.jsx
+++ src/components/MusicPlayer/Track.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Track = () => {
   return (
-    <div className='bg-red-600 grid col-span-2 w-full'>Track</div>
+    <div className='bg-red-600 grid col-span-2 w-full grid-flow-col items-center justify-center'>
+      <div className='hidden sm:block h-16 w-16 mr-4'>
+        <img src="/logo1.jpg" alt="Song img" className=' rounded-full'/>
+      </div>
+      <div>
+        <p>Song Name</p>
+        <p>Artist name </p>
+      </div>
+    </div>
   )
 }
 
