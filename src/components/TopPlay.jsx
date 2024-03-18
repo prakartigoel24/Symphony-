bf5f078744d94cb7bd1e93ac127bac53871d019a
Prakarti Goel <prakartigoel24@gmail.com>
2024-03-18 21:54:50 +0530

    Added song play on top songs click feat

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -7,8 +7,19 @@ import "swiper/css/free-mode";
 import { Link } from "react-router-dom";
 import Loader from "./Loader";
 import ErrorComponent from "./ErrorComponent";
+import PlayPause from "./PlayPause";
 
-const TopSongCard = ({ song, i }) => {
+import { playPause, setActiveSong } from "../redux/Slices/PlayerSlice";
+import { useDispatch, useSelector } from "react-redux";
+
+const TopSongCard = ({
+  song,
+  i,
+  isPlaying,
+  activeSong,
+  handlePauseClick,
+  handlePlayClick,
+}) => {
   return (
     <div className="flex flex-row w-full items-center rounded-lg mb-2 py-2 cursor-pointer">
       <h1 className="font-bold text-base text-white mr-3">{i + 1}.</h1>
@@ -19,9 +30,18 @@ const TopSongCard = ({ song, i }) => {
           alt={song?.title}
         />
         <div className="flex flex-col justify-center mx-3">
-          <p className="text-md md:text-lg font-bold text-white">{song?.title}</p>
+          <p className="text-md md:text-lg font-bold text-white">
+            {song?.title}
+          </p>
           <p className="text-base mt-1 text-gray-300">{song?.subtitle}</p>
         </div>
+        <PlayPause
+          isPlaying={isPlaying}
+          activeSong={activeSong}
+          song={song}
+          handlePause={handlePauseClick}
+          handlePlay={handlePlayClick}
+        />
       </div>
     </div>
   );
@@ -30,6 +50,19 @@ const TopSongCard = ({ song, i }) => {
 const TopPlay = () => {
   const { data: topSongs, isFetching, isError } = useGetTopSongsQuery();
   const topFiveSongs = topSongs?.tracks?.slice(0, 5);
+
+  const dispatch = useDispatch();
+
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  const handlePlayClick = (song, i) => {
+    dispatch(setActiveSong({ song, data: topSongs, i }));
+    dispatch(playPause(true));
+  };
 
   if (isFetching) return <Loader />;
   if (isError) return <ErrorComponent />;
@@ -43,7 +76,17 @@ const TopPlay = () => {
         </div>
         <div className="mt-4 flex flex-col">
           {topFiveSongs?.map((song, i) => {
-            return <TopSongCard song={song} i={i} key={song?.key} />;
+            return (
+              <TopSongCard
+                song={song}
+                i={i}
+                key={song?.key}
+                isPlaying={isPlaying}
+                activeSong={activeSong}
+                handlePauseClick={handlePauseClick}
+                handlePlayClick={() => handlePlayClick(song, i)}
+              />
+            );
           })}
         </div>
       </div>
